99e95a9357d0e6c6658c1601c1d40a97c24795ba
ben-dev10 <calvinklien025@gmail.com>
2023-10-31 15:50:15 +0000

    - added navigation menu (radix)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import { createPortal } from "react-dom";
 import AccordionParent from "./accordion/accordion-parent";
 import SelectButton from "./select-button";
+import NavigationMenuST from "./NavigationMenu";
+
 
 function App() {
   return (
     <>
       {createPortal(<AccordionParent />, document.getElementById("accordion"))}
+      {createPortal(<NavigationMenuST />, document.getElementById("nav-menu"))}
       {createPortal(<SelectButton />, document.getElementById("select-button"))}
       {createPortal(
         <AccordionParent />,
